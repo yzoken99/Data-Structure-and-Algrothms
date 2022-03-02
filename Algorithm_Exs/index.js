c3180e071e0870b0ee2755d0e47a1706e93aa381
Izzatillo <james1999007@gmail.com>
2022-03-02 15:43:09 +0100

    updated

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -1,11 +1,49 @@
-function simpleArraySum(ar) {
-    let counter = 0;
-    for(let i = 0; i <= ar.length; i++){
-        if(counter >= 0 && ar[i] <= 1000){
-            counter += ar[i]
+// function isSubsequence(s, t) {
+//     for(let i = 0; i <= s.length; i++){
+//         for(let j = 0; j <=t.length; j++){
+//             if(t[j].includes(s[i])){
+//                 console.log(s[i]);
+//                 console.log(t[j]);
+//                 return true
+//             }else{
+//                 return false
+//             }
+//         }
+//     }
+// };
+// console.log(isSubsequence("axc", "ahbgdc"))
 
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+let twoSum = function(nums, target) {
+    let result = []
+    for(let i = 0; i <= nums.length; i++){
+        for(let j = 1; j <= nums.length; j++){
+            if(nums[i] + nums[j] === target){
+                result.push(i)
+                result.push(j)
+                return result
+            }
         }
     }
-    return counter
-}
-console.log(simpleArraySum([1, 2, 3, 4, 10, 11, 12]))
+};
+console.log(twoSum([2,5,5,15], 10))
+
+let isPalindrome = function(x) {
+    for(let i = 0; i <= x.toString().length; i++){
+        for(let j = x.toString().length-1; j >= 0; j--){
+            console.log("Xj", j);
+            if(x[i] === x[j]){
+                return true
+            }else{
+                return false
+                // console.log(x.toString()[j]);
+            }
+        }
+        
+    }
+};
+// console.log(isPalindrome(121))
+console.log(isPalindrome(1000021))
