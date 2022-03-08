4927e43b96ce5f1a228344ad567f9b708cccd3a9
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-03-08 14:59:36 +0100

    updated

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -1,17 +1,7 @@
+let n = 6
+for (let i = 0; i <= n; i++) {
+    console.log('   ' + i);
 
-// Input: nums = [3,1,2,2,2,1,3], k = 2
-let countPairs = function(nums, k) {
-    nums.sort()
-    console.log("nums", nums);
-    let counter = 0;
-    for(let i = 0; i < nums.length; i++ ){
-        for(let j = 1; j < nums.length; j++){
-            console.log("J", nums[j]);
-            if(nums[i] === nums[j] && (i*j)%k === 0){
-                counter++
-            }
-        }
-    }
-    return counter;
-};
-console.log(countPairs(nums = [3,1,2,2,2,1,3], k = 2))
+}
+
+
