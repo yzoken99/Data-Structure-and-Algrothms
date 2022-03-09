025c14b526793f7b971f46ade681a7d9cb48e5e1
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-03-09 13:12:19 +0100

    updated

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -1,7 +1,17 @@
-let n = 6
-for (let i = 0; i <= n; i++) {
-    console.log('   ' + i);
+function birthdayCakeCandles(candles) {
+    // Write your code here
+    let maxCandles = 0;
+    let counter = 0;
+    for(let i = 0; i <= candles.length; i++){
+        if(candles[i] >= maxCandles){
+            maxCandles = candles[i]
+            if(){
+                
+            }
+        }
+    }
+    return maxCandles
 
 }
-
+console.log(birthdayCakeCandles([3, 2, 1, 3, 3, 3, 4]))
 
