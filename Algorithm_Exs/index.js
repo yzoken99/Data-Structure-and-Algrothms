cf0c0a8c277885bb5a3e81172afadd0b04576949
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-03-15 14:18:23 +0100

    updated

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -4,20 +4,25 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
   let appArr = [];
   let orangeArr = [];
   for (let i = 0; i < apples.length; i++) {
-    appArr.push(a + apples[i]);
+    if(a >= 1 && a <= Math.pow(10, 5)){
+      appArr.push(a + apples[i]);
+    }
   }
   for (let m = 0; m < appArr.length; m++) {
-    if (appArr[m] >= s && appArr[m] <= t) {
+    if (s,t >= 1 && s,t <= Math.pow(10, 5) && appArr[m] >= s && appArr[m] <= t) {
       landedApple += m;
     }
   }
 
   for (let j = 0; j < oranges.length; j++) {
-    orangeArr.push(b + oranges[j]);
+    if(b >= 1 && b <= Math.pow(10, 5)){
+      orangeArr.push(b + oranges[j]);
+
+    }
   }
 
   for (let n = 0; n < orangeArr.length; n++) {
-    if (orangeArr[n] > s && orangeArr[n] < t) {
+    if (s,t >= 1 && s,t <= Math.pow(10, 5) && orangeArr[n] > s && orangeArr[n] < t) {
       landedOrange += n;
     }
   }
