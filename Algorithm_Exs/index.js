720b02f69731c6a989fe81feb3028245ea5e3732
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-03-19 23:49:45 +0100

    updated

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -1,38 +1,52 @@
-// function oddNumbers(l, r) {
-//   let arr = [];
-//   while (l <= r) {
-//       arr.push(l);
-//       l += 1;
-//   };
-//   return arr.filter(n => n % 2);
+// function myString(str1, str2){
+//     for(let i = 0; i < str1.length; i++){
+//         for(let j = 0; j < str1[i].length; j++){
+//             for(let m = 0; m < str2.length; m++){
+//                 for(let n = 0; n < str2[m].length; n++){
+//                     if(str1[i].includes(str2[m][n] || str2[m].includes(str1[i][j]))){
+//                         console.log("YES")
+//                     }else{
+//                         console.log("no")
+//                     }
+//                 }
+
+//             }
+
+//         }
+//     }
+    
 // }
-// console.log(oddNumbers(2,5))
-
-// function commonSubstring(a) {
-//    for( let i in a){
-//        for(let j = 0; j <= a[i].length; j++){
-//         console.log(j);
-//        }
-//    }
-
-// }
-// commonSubstring(["hello", "hi"])
-
-// , ["world", "bye"]
+// console.log(myString(["hello", "world"], ["level", "asq"]))
 
 
 
-let str1 = "Hello"
-let str2 = "World"
-let res = str1.split('')
-function myString(str1){
-    // for(let i = str1.length-1; i >=0; i--){
-    //     console.log(str1[i]);
-    // }
+// return str1.split('').reduce((acc, val)=> {
+//      acc = val + acc
+//      return acc
+//  },'')
 
-   return str1.split('').reduce((acc, val)=> {
-        acc = val + acc
-        return acc
-    },'')
+
+
+
+
+function countDuplicates(numbers) {
+    let res = []
+    for(let i = 0; i <= numbers.length; i++){
+        for(let j = 1; j <= numbers.length; j++){
+            if(numbers[i] == numbers[j]){
+                res.push(numbers[i])
+                // res.push()
+            }
+        }
+    }
+    return res
+
 }
-console.log(myString("Hello"))
+console.log(countDuplicates([1,1,2,2,2,3]))
+
+
+
+
+
+
+
