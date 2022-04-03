9f400a461a70a1bf26b9a0a6d46fa906951bbfa3
Izzatillo <91114422+alimukhtor@users.noreply.github.com>
2022-04-03 11:58:00 +0200

    solved plus minus problem

--- Algorithm_Exs/index.js
+++ Algorithm_Exs/index.js
@@ -1,18 +1,39 @@
-function miniMaxSum (arr){
-  let min = 0;
-  let max = 0;
-  let sumMax = 0;
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] > max){
-      max = arr[i]
-    }
-    if(arr[i] === max){
-      sumMax += arr[i]
+// function miniMaxSum (arr){
+//   let min = 0;
+//   let max = 0;
+//   let sumMax = 0;
+//   for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > max){
+//       max = arr[i]
+//     }
+//     if(arr[i] === max){
+//       sumMax += arr[i]
 
-    }
+//     }
     
     
+//   }
+//   return sumMax
+// }
+// console.log(miniMaxSum([1,2,3,4,5]))
+
+
+
+function plusMinus(arr) {
+  let minusInt = 0;
+  let plusInt = 0;
+  let zeroInt = 0;
+  let arrLength = arr.length
+  for(let i = 0; i <= arr.length; i++){
+      if(arr[i] > 0){
+          plusInt++
+      }else if(arr[i] < 0){
+          minusInt++
+      }else if(arr[i] === 0){
+          zeroInt++
+      }
   }
-  return sumMax
+  console.log((plusInt/arrLength).toFixed(6).substring(-1) + "\n" +  (minusInt/arrLength).toFixed(6).substring(-1) + "\n" + (zeroInt/arrLength).toFixed(6).substring(-1))
+
 }
-console.log(miniMaxSum([1,2,3,4,5]))
+plusMinus([1,1,0,-1,-1])
